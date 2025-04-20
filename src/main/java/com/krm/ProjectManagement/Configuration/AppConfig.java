1236e00b13f6c6fa167bbf12e5d07f1223a2e578
KishoreRam-M <mrkisho65@outlook.com>
2025-04-20 22:38:26 +0530

    feat: Security Filter chain Method  Add

--- src/main/java/com/krm/ProjectManagement/Configuration/AppConfig.java
+++ src/main/java/com/krm/ProjectManagement/Configuration/AppConfig.java
@@ -1,4 +1,24 @@
 package com.krm.ProjectManagement.Configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
 public class AppConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf->csrf.disable())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorithy->authorithy.requestMatchers("/api/**").authenticated()
+                        .requestMatchers("*").permitAll())
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .cors(cors ->cors.configurationSource(coresConfigurationSource()));
+        return httpSecurity.build();
+    }
 }

2c54e58f1dcd63f4205fddaf79bc767df46f6738
KishoreRam-M <mrkisho65@outlook.com>
2025-04-21 23:06:10 +0530

    feat:  i thing security is completed  jwt is working correctly

--- src/main/java/com/krm/ProjectManagement/Controller/AuthController.java
+++ src/main/java/com/krm/ProjectManagement/Controller/AuthController.java
@@ -1,37 +1,82 @@
 package com.krm.ProjectManagement.Controller;
 
+import com.krm.ProjectManagement.Configuration.JwtProvider;
+import com.krm.ProjectManagement.Model.AuthResponse;
+import com.krm.ProjectManagement.Model.LoginRequest;
 import com.krm.ProjectManagement.Model.User;
 import com.krm.ProjectManagement.Repo.UserRepo;
 import com.krm.ProjectManagement.Service.CustomUserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
     @Autowired
     private UserRepo userRepo;
+
     @Autowired
     private PasswordEncoder passwordEncoder;
+
     @Autowired
     private CustomUserDetailsImpl customUserDetails;
-@PostMapping("/signup")
-    public ResponseEntity<User> createUserHandler(@RequestBody User user) throws Exception {
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws Exception {
         User isUserExist = userRepo.findByEmail(user.getEmail());
         if (isUserExist != null) {
-            throw new Exception("email already exist with another  account");
+            throw new Exception("Email already exists with another account");
         }
-        User createUser = new User();
-        createUser.setPassword(passwordEncoder.encode(user.getPassword()));
-        createUser.setEmail(user.getEmail());
-        createUser.setName(user.getName());
-        User savedUser = userRepo.save(createUser);
-        return ResponseEntity.ok(createUser);
 
+        User newUser = new User();
+        newUser.setEmail(user.getEmail());
+        newUser.setName(user.getName());
+        newUser.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        User savedUser = userRepo.save(newUser);
+
+        // Authenticate after signup
+        Authentication authentication = authenticate(user.getEmail(), user.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = JwtProvider.generateToken(authentication);
+        AuthResponse authResponse = new AuthResponse(jwt, "Signup successful");
+
+        return ResponseEntity.ok(authResponse);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signIn(@RequestBody LoginRequest loginRequest) {
+        String username = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+
+        Authentication authentication = authenticate(username, password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = JwtProvider.generateToken(authentication);
+        AuthResponse authResponse = new AuthResponse(jwt, "Signin successful");
+
+        return ResponseEntity.ok(authResponse);
+    }
+
+    private Authentication authenticate(String username, String password) {
+        UserDetails userDetails = customUserDetails.loadUserByUsername(username);
+
+        if (userDetails == null) {
+            throw new BadCredentialsException("User not found");
+        }
+
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new BadCredentialsException("Invalid password");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
 }
